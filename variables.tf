5f4721cff572cdb33193b95db945b222a4d3fef1
Treasure Muriithi <treasure.k.muriithi@gmail.com>
2025-08-03 13:31:36 +0100

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,16 @@
+variable "aws_region" {
+  default = "eu-west-2"
+}
+
+variable "aws_az" {
+  default = "eu-west-2a"
+}
+
+variable "ami_id" {
+  description = "Ubuntu AMI ID"
+  default     = "ami-0cbe2951c5f556bb1" # Ubuntu 22.04 LTS for eu-west-2
+}
+
+variable "key_name" {
+  description = "Name of your EC2 key pair"
+}
