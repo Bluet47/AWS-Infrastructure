8105143edf0492e204b444d1f4050e97c09dc51c
Treasure Muriithi <treasure.k.muriithi@gmail.com>
2025-08-04 12:05:13 +0100

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -20,12 +20,13 @@ docker pull splunk/attack_range
 echo "[INFO] Running Attack Range container..."
 docker run -d \
   --name attack_range \
-  -p 8000:8000 \  # Splunk Web
-  -p 8080:8080 \  # Guacamole
-  -p 443:443 \    # HTTPS if needed
-  -p 9997:9997 \  # Splunk Forwarder
-  -p 22:22 \      # SSH (optional, if container supports)
-  -p 8089:8089 \  # Splunk Management Port
+  -p 8000:8000 \
+  -p 8080:8080 \
+  -p 443:443 \
+  -p 9997:9997 \
+  -p 22:22 \
+  -p 8089:8089 \
   splunk/attack_range
 
 echo "[INFO] Startup complete."
+
